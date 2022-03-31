a8f48101fd697f2d00bdbb71112d99185328b69d
PeeyushPrashant <prashantpeeyush3003@gmail.com>
2022-03-31 18:45:47 +0530

    feat: Added labels

--- src/context/data-context.js
+++ src/context/data-context.js
@@ -26,10 +26,11 @@ const DataProvider = ({ children }) => {
     _id: "",
     title: "",
     body: "",
-    backGround: "#e7dcdc",
+    backGround: "#192129",
     CreatedAt: `${String(date.getDate()).padStart(2, "0")}/${String(
       date.getMonth() + 1
     ).padStart(2, "0")}/${date.getFullYear()}`,
+    tag: "",
   };
   const [noteState, noteDispatch] = useReducer(noteReducer, { noteData: [] });
   const [archiveState, archiveDispatch] = useReducer(archiveReducer, {
